a092e1b30bdcc1b17deea996ccde5c7a9288b2e2
Shivani Pal <shiv040998@gmail.com>
2023-07-02 11:40:45 -0400

    Logging and exception handling

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+'''
+custom exception handling
+'''
+
+
+def error_message_detail(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = "ERROR OCCURED IN PYHTON SCRIPT [{0}] IN LINE NUMBER [{1}] AND ERROR MESSAGE IS : [{2}]".format(
+        file_name, line_number, str(error)
+    )
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
